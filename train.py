8b723e98827059e487d8c576bcd39ee280dd7f23
Babur <baburnawyan@gmail.com>
2025-03-23 15:59:02 -0400

    Finalized config settings

--- train.py
+++ train.py
@@ -5,16 +5,6 @@ from tamer.datamodule import HMEDatamodule
 from tamer.lit_tamer import LitTAMER
 
 
-# torch.backends.cuda.max_split_size_mb = 64
-#
-# # enable cudnn optimization
-# torch.backends.cudnn.benchmark = True
-# torch.backends.cudnn.deterministic = False
-#
-# # force cuDNN to find a valid convolution algorithm
-# torch.backends.cudnn.enabled = True
-
-# def main():
 cli = LightningCLI(
           LitTAMER,
           HMEDatamodule,
@@ -25,8 +15,4 @@ cli = LightningCLI(
               'auto_scale_batch_size': 'binsearch',  # dynamically reduce batch size
               'gpus': 1,
           }
-      )
-
-#
-# if __name__ == "__main__":
-#     main()
+)
