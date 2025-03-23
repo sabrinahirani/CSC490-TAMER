df446593d6c5c732c5e94d230146fb7d0c597418
Babur <baburnawyan@gmail.com>
2025-03-22 21:26:49 -0400

    Set num_workers to 0

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 # from pytorch_lightning.plugins.training_type.ddp import DDPPlugin
 from pytorch_lightning.utilities.cli import LightningCLI
-from pytorch_lightning import Trainer
+# from pytorch_lightning import Trainer
 
 from tamer.datamodule import HMEDatamodule
 from tamer.lit_tamer import LitTAMER
@@ -8,7 +8,7 @@ from tamer.lit_tamer import LitTAMER
 cli = LightningCLI(
     LitTAMER,
     HMEDatamodule,
-    trainer_class=Trainer,
+    # trainer_class=Trainer,
     save_config_overwrite=True,
     # trainer_defaults={"plugins": DDPPlugin(find_unused_parameters=False)},
 )
